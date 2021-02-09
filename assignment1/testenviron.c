6af46602cccf0877ea6969e761cabc92f2ddb5a2
Farah Babu <babu@hoare7.cs.umsl.edu>
2021-02-09 10:57:51 -0600

    working with hardcodings

--- assignment1/testenviron.c
+++ assignment1/testenviron.c
@@ -4,6 +4,37 @@
 
 extern char **environ;
 
+char *substring(char *string, int position, int length)
+{
+   char *p;
+   int c;
+ 
+   p = malloc(length+1);
+   
+   if (p == NULL)
+   {
+      printf("Unable to allocate memory.\n");
+      exit(1);
+   }
+ 
+   for (c = 0; c < length; c++)
+   {
+      *(p+c) = *(string+position-1);      
+      string++;  
+   }
+ 
+   *(p+c) = '\0';
+ 
+   return p;
+}
+
+char *environSubString(char *namevalue, int n){
+	char *new = malloc(sizeof(char)*n+1);
+	strncpy(new, namevalue, n);
+	new[n] = '\0';
+	return new;
+}
+
 void printsomething(const char *c){
     int len = strlen(c);
     char *env_var;
@@ -13,6 +44,32 @@ void printsomething(const char *c){
     //printf("test char :%s : %s : %s ---> %i\n",c, getenv("OLDPWD"), c, len);
     printf("Variable %s:%s\n", env_var, getenv(env_var));
 }
+
+char *getMyEnv(const char *name){
+     int i = 0;
+     char *p;
+     while(environ[i]){
+       if(!strncmp(environ[i], name, strlen(name))){
+            //return(environ[i] + strlen(name) + 1);
+            //printf("equals appear at %s", strchr(environ[i], '='));
+            printf("strlen(environ): %zu strlen(name): %zu, strlen 2: %zu, length to = %zu \n", strlen(environ[i]), strlen(name), strlen(strchr(environ[i], '=')), (strlen(environ[i]) - strlen(strchr(environ[i], '='))));
+            //p = substring(environ[i], 0, strlen(name) - strlen(strchr(environ[i], '=')));
+            //printf("The name %s", p);
+            //free(p);
+            //printf("Name of the variable: %s\n", environSubString(environ[i], strlen(name)));
+            printf("Name of the variable: %s\n", environSubString(environ[i], (strlen(environ[i]) - strlen(strchr(environ[i], '=')))));
+	    printf("Get Env : %s\n", getenv(environSubString(environ[i], (strlen(environ[i]) - strlen(strchr(environ[i], '='))))));
+	    return(environ[i]);
+       }
+       i++;
+     }
+     return NULL;
+}
+
+
+//char printEnvironmentVariable(const char *name){
+//
+//}
 
 void updateEnvironmentVariable(char *namevalue){
     putenv(namevalue);
@@ -50,8 +107,9 @@ int main() {
  
   getchar();
   printf("********\n");
-  clearAllEnvironmentVariables("PASSCODE2=TEST");
+  clearAllEnvironmentVariables("PASSCODE2=TEST=TEST2");
   printAllEnvironmentVariables();
+  printf("MY VAR :%s", getMyEnv("PASSCODE2"));
 
   getchar();
   updateEnvironmentVariable("TZ=est5est");
