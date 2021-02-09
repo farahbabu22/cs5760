ab09666e4e1106c36df3ba33a7043de3dc5fd60b
Farah Babu <babu@hoare7.cs.umsl.edu>
2021-02-09 01:08:06 -0600

    attempt at testenviron.c

--- assignment1/testenviron.c
+++ assignment1/testenviron.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 extern char **environ;
 
@@ -7,9 +9,21 @@ int main() {
   char *s = *environ;
   int i = 1;
 
+  const char eq[2] = "=";
+  char *token;
+  char *val;
+  char *variable;
+ 
 
   while(s){
-    printf("%s\n", s);
+    //printf("%s\n", s);
+    token = strtok(s, eq);
+    val = malloc(strlen(token)+1);
+    strcpy(val, token);
+    //char *c = getenv(val);
+    //char *c = getenv("SSH_CLIENT");
+    //variable = getenv(x*token);
+    printf("%s:%s:%s\n", token, getenv("PATH"), val);
     s = *(environ+i);
     i++;
   }
